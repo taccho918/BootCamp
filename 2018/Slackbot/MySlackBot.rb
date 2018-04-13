5b87e13de0dbe9717b1e8ea5ece6e5921c65191d
Momoka Takahashi <highbridge918@gmail.com>
2018-04-13 09:03:31 +0200

    Add MySlackBot.rb for heroku

--- 2018/Slackbot/MySlackBot.rb
+++ 2018/Slackbot/MySlackBot.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'sinatra'
@@ -5,6 +6,16 @@ require 'SlackBot'
 
 class MySlackBot < SlackBot
   # cool code goes here
+  # "@Bot「○○」と言って" -> "@user_name ○○"
+  def respond_msg(params, options = {})
+    return nil if params[:user_name] == "slackbot" || params[:user_id] == "USLACKBOT"
+
+    msg = params[:text]
+    msg = msg.match(/「(.*)」と言って/)
+    msg = msg[1]
+    user_name = params[:user_name] ? "@#{params[:user_name]}" : ""
+    return {text: "#{user_name} " + msg}.merge(options).to_json
+  end  
 end
 
 slackbot = MySlackBot.new
@@ -17,5 +28,7 @@ end
 
 post '/slack' do
   content_type :json
-  slackbot.naive_respond(params, username: "Bot")
+  
+  # slackbot.naive_respond(params, username: "TakaBot")
+  slackbot.respond_msg(params, username: "TakaBot")
 end
